ec1efc60f8d54ef9d04bca04a0fec06b6e02dd5b
E021M <edumoura1234@gmail.com>
2021-09-15 18:31:50 -0300

    Mostrar erros e mensagens

--- resources/views/components/body.blade.php
+++ resources/views/components/body.blade.php
@@ -4,11 +4,25 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             @yield('name-page')
         </h2>
+        @if (session('message'))
+            <div class="alert alert-info" role="alert">
+                <p>{{ session('message') }}</p>
+            </div>            
+        @endif
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                @if ($errors->any())
+                    <div class="alert alert-danger" role="alert">
+                        <ul class="text-danger">
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
                 @yield('content')
             </div>
         </div>
