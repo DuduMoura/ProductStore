6e3466daec09d5060efba10c9b7ab5e8d38fa372
E021M <edumoura1234@gmail.com>
2021-09-15 13:23:43 -0300

    Criado migration, model e controller dos Produtos

---
+++ database/migrations/2021_09_15_131326_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('code');
+            $table->double('price', 10, 2);
+            $table->unsignedBigInteger('shops_id')->nullable();
+            $table->timestamps();
+            
+            $table->foreign('shops_id')->references('id')->on('shops');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
