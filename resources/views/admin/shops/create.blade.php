ad46d02c2113a47dc8d02285b6572e884fd3a79e
E021M <edumoura1234@gmail.com>
2021-09-16 09:44:48 -0300

    Recurso repetido removido

--- resources/views/admin/shops/create.blade.php
+++ resources/views/admin/shops/create.blade.php
@@ -5,7 +5,6 @@
     <div class="row">
         <div class="col-md-12">
             <form action="{{ route('admin.shops.store') }}" method="POST">
-                @csrf
                 @include('admin.shops.components.form')
             </form>
         </div>
