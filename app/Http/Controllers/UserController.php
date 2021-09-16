aa18cf386c507cbf10e9c755a7f5e480ac8b1712
E021M <edumoura1234@gmail.com>
2021-09-16 14:19:41 -0300

    Removido Request desnecessário

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Shop;
-use Illuminate\Http\Request;
 use App\Http\Requests\Auth\RegisterRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
