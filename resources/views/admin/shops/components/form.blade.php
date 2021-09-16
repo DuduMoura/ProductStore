51f99cdd1addf7b23ec3850d4513e0a313c122d7
E021M <edumoura1234@gmail.com>
2021-09-16 12:10:23 -0300

    Campos obrigatórios no formulário

--- resources/views/admin/shops/components/form.blade.php
+++ resources/views/admin/shops/components/form.blade.php
@@ -2,13 +2,13 @@
 <div class="col-md-12">
     <div class="form-group">
         <label><span class="text-danger">*</span> Nome</label>
-        <input class="form-control @if ($errors->has('name')) is-invalid @endif" type="text" value="{{ $shop->name ?? old('name') }}" name="name" id="name"/>
+        <input class="form-control @if ($errors->has('name')) is-invalid @endif" type="text" value="{{ $shop->name ?? old('name') }}" name="name" required id="name"/>
     </div>
 </div>
 <div class="col-md-12">
     <div class="form-group">
         <label><span class="text-danger">*</span> Endereço</label>
-        <input class="form-control @if ($errors->has('address')) is-invalid @endif" type="text" value="{{ $shop->address ?? old('address') }}" name="address" id="address"/>
+        <input class="form-control @if ($errors->has('address')) is-invalid @endif" type="text" value="{{ $shop->address ?? old('address') }}" name="address" required id="address"/>
     </div>
 </div>
 <div class="col-md-12 mb-2">
